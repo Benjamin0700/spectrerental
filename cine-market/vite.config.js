172836d6909eee0ad20a33b021b7cac7144dc152
Benjamin <RichMan@MacBook-Pro-RichMan.local>
2025-03-11 17:07:08 +0500

    done

--- cine-market/vite.config.js
+++ cine-market/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/", // Agar custom domen ishlatayotgan bo'lsangiz, to'g'ri ekanini tekshiring
   plugins: [react()],
+  base: "/cine-market/", // Change this to match your repo name
 });
